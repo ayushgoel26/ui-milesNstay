9fc55809d7c7316f9c27230ebf121194c06bd074
Reva <revagupta95@gmail.com>
2022-09-17 14:59:49 -0500

    Carousel added

--- mns.js
+++ mns.js
@@ -9,22 +9,22 @@
 //   }
 // }
 
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.getElementById("navbarsearch").style.top = "0";
-  } else {
-    document.getElementById("navbarsearch").style.top = "-130px";
-  }
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+//     document.getElementById("navbarsearch").style.top = "0";
+//   } else {
+//     document.getElementById("navbarsearch").style.top = "-130px";
+//   }
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-50px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
 
 // /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 // var prevScrollpos = window.pageYOffset;
@@ -37,3 +37,22 @@ function scrollFunction() {
 //   }
 //   prevScrollpos = currentScrollPos;
 // }
+
+// Carousel Script
+// let items = document.querySelectorAll('.carousel .carousel-item')
+// console.log(items)
+
+// items.forEach((el) => {
+//   console.log("hi")
+//     const minPerSlide = 4
+//     let next = el.nextElementSibling
+//     for (var i=1; i<minPerSlide; i++) {
+//         if (!next) {
+//             // wrap carousel by using first child
+//             next = items[0]
+//         }
+//         let cloneChild = next.cloneNode(true)
+//         el.appendChild(cloneChild.children[0])
+//         next = next.nextElementSibling
+//     }
+// })
