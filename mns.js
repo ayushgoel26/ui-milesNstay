ee6cfdf7f8835552df03276926d0284bb481c9f0
Reva <revagupta95@gmail.com>
2022-09-18 17:22:33 -0500

    Code cleaning html and js

--- mns.js
+++ mns.js
@@ -1,14 +1,4 @@
-// // When the user scrolls down 50px from the top of the document, resize the header's font size
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-//     document.getElementById("header").style.fontSize = "30px";
-//   } else {
-//     document.getElementById("header").style.fontSize = "90px";
-//   }
-// }
-
+// Navbar Script
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
@@ -47,36 +37,3 @@ function scrollFunction() {
 	// }
 	// prevScrollpos = currentScrollPos;
 }
-
-
-
-// /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-//   var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.getElementById("navbar").style.top = "0";
-//   } else {
-//     document.getElementById("navbar").style.top = "-50px";
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
-
-// Carousel Script
-// let items = document.querySelectorAll('.carousel .carousel-item')
-// console.log(items)
-
-// window.onload = () => { items.forEach((el) => {
-//   console.log("hi")
-//     const minPerSlide = 4
-//     let next = el.nextElementSibling
-//     for (var i=1; i<minPerSlide; i++) {
-//         if (!next) {
-//             // wrap carousel by using first child
-//             next = items[0]
-//         }
-//         let cloneChild = next.cloneNode(true)
-//         el.appendChild(cloneChild.children[0])
-//         next = next.nextElementSibling
-//     }
-// })};
